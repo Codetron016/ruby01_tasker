d9fb195653f4a420c723b0186f008008d6824b4f
Davis Sam <shotime013@gmail.com>
2017-10-12 16:46:29 +0000

    added new template  and route for creating new tasks

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  
+  resources :tasks
+  
   root 'pages#home' #pages controller with home action.
   
   get 'about' => 'pages#about' # additional pages.
