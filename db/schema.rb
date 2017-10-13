e970f6628bd9976bf770bac96d4f46f2d95960a8
Davis Sam <shotime013@gmail.com>
2017-10-13 10:20:18 +0000

    added state logic and change index UI

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013065710) do
+ActiveRecord::Schema.define(version: 20171013074113) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "user_id"
+    t.string   "state",      default: "to_do"
   end
 
   create_table "users", force: :cascade do |t|
